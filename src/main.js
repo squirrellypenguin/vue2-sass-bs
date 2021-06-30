e42e4850fd1fd766b7a97575827257b2c3e7a96c
Ted Teah <from@dabronx.nyc>
2021-06-30 15:57:52 -0400

    first commit

--- src/main.js
+++ src/main.js
@@ -1,11 +1,26 @@
-import Vue from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
-import router from './router'
+import Vue from "vue";
+import router from "./router";
+// import "bootstrap/dist/css/bootstrap.min.css"
+// import "bootstrap-vue/dist/bootstrap-vue.css"
+// import BootstrapVue from "bootstrap-vue"
+// import InfoCard from "vue-info-card"
+// import { fab } from '@fortawesome/free-brands-svg-icons'
+// import { library } from '@fortawesome/fontawesome-svg-core'
+// import { faUserSecret, faGem, faEnvelopeOpenText } from '@fortawesome/free-solid-svg-icons'
+// import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+// library.add(fab, faUserSecret, faGem, faEnvelopeOpenText)
+// // import Buefy from 'buefy'
+// // import 'buefy/dist/buefy.css'
+// Vue.component('font-awesome-icon', FontAwesomeIcon)
+// Vue.use(BootstrapVue)
+// Vue.use(InfoCard)
+// // Vue.use(Buefy)
 
-Vue.config.productionTip = false
+// Vue.config.productionTip = false;
 
 new Vue({
   router,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
